6d4310c6392a827068973f4dd0990dd5c85f7ea2
1gaojer <113914089+1gaojer@users.noreply.github.com>
2022-09-19 10:11:31 -0400

    Update perimeter_of_triangle.py

--- perimeter_of_triangle.py
+++ perimeter_of_triangle.py
@@ -1,7 +1,7 @@
 def perimeter_of_triangle():
-    a= float(input("side a: "))
-    b= float(input("side b: "))
-    c= float(input("side c: "))
+    a = float(input("side a: ")) # asks user to input each side length and converts to float type
+    b = float(input("side b: "))
+    c = float(input("side c: "))
     
     print(a+b+c)
     
