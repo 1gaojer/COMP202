afe12ea89e8886bfcef3accb949f42b46eac22fd
1gaojer <113914089+1gaojer@users.noreply.github.com>
2022-09-19 10:07:53 -0400

    Update area_of_triangle.py

--- area_of_triangle.py
+++ area_of_triangle.py
@@ -1,13 +1,13 @@
 import math
 
 def area_of_triangle():
-    a= float(input("side a: "))
-    b= float(input("side b: "))
-    c= float(input("side c: "))
+    a = float(input("side a: ")) # asks user for lenght of each side and converts to float type
+    b = float(input("side b: "))
+    c = float(input("side c: "))
     
-    per = float((a + b + c)/2)
+    per = float((a + b + c)/2) # calculates half the perimeter
     
-    area = round((math.sqrt((per * (per-a) * (per-b) * (per-c)))),2)
+    area = round((math.sqrt((per * (per-a) * (per-b) * (per-c)))),2) # uses goven function to calculate area to two decimals
   
     print(area)
     
