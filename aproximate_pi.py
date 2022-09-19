034e31a8e199fa721da2098be4405e5a26931fad
1gaojer <113914089+1gaojer@users.noreply.github.com>
2022-09-19 10:02:56 -0400

    Variable half_apothem should be renamed
    Apothem refers to the height, not the base I think.

--- aproximate_pi.py
+++ aproximate_pi.py
@@ -1,12 +1,11 @@
 import math
-def aproximate_pi(n):
-    theta = math.radians(360/n)
-    height = ((math.cos((theta/2))))
-    half_apothem = (math.sin((theta/2)))
+def aproximate_pi(n): # takes an integer >2 number of sides as parameter 'n'
+    theta = math.radians(360/n) # calculates, in radians, the central angle of each partitioned triangle
+    height = ((math.cos((theta/2)))) # calcules the height of the triangle
+    half_apothem = (math.sin((theta/2))) # calculates half the base of the triangle
     
     
-    
-    area = (n * height * half_apothem)
+    area = (n * height * half_apothem) #calculates the area using simplied formula of a triangle times n triangles
    
     print(area)
      
